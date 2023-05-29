9d35dd931b5caffdde2619f58cce81a7852275bc
jeremypenafiel <japenafiel@up.edu.ph>
2023-05-29 15:12:25 +0800

    navbar + js
    added login and account page functionality

---
+++ Main/JS/accountPage.js
@@ -0,0 +1,25 @@
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { auth, db } from "./connect.js";
+
+
+async function getAccountDetails(auth) {
+    auth.onIdTokenChanged(async (user) => {
+        if (user) {
+            const account = doc(db, "Accounts", user.uid);
+            const snap = await getDoc(account);
+            const data= snap.data();
+            addAccountDetails(data);
+        } else {
+            return null;
+        }
+    });
+}
+function addAccountDetails(detailsArray) {
+    document.getElementById("name").innerHTML = detailsArray["name"];
+    document.getElementById("email").innerHTML = detailsArray["email"];
+}
+
+window.addEventListener("load", async () => {
+    const array = await (getAccountDetails(auth));
+
+});
